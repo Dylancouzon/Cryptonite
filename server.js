17c8c33e33bf1c1096bad140374f2f0e8c02975c
Dylancouzon <dylancouzon@gmail.com>
2021-05-24 15:01:52 -0700

    Add sessions

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const app = express();
 const mongoose = require("mongoose");
+var session = require('express-session');
 const { Users } = require("./models");
 let userData = require('./seeds/users.json');
-const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cryptousers", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
+const app = express();
+
+//Initialize Express session
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+  }))
+
 // Visit the /seed get route to populate the db with data from the users.json file.
 app.get("/seed", async (_req, _res) => {
     //Hash the passwords
-    // const hashedUsers = userData.map(async (user) => console.log(user.password));
+    //const bcrypt = require('bcrypt');
+    //  const hashedUsers = await userData.map((user) => bcrypt.hash(user.password, 10));
     Users.create(userData)
         .then(dbSeeded => {
             console.log(dbSeeded);
