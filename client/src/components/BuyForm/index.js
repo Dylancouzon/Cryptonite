7950c5b584453f21f8d4f7d1b0b2855a7d7cbe43
Jake Novelli <jakenovelli11@gmail.com>
2021-05-24 15:59:07 -0700

    adjusted buyform sizes

--- client/src/components/BuyForm/index.js
+++ client/src/components/BuyForm/index.js
@@ -9,7 +9,7 @@ function BuyForm() {
                     <Form.Label column md={4}>
                         Amount of Coins:
                         </Form.Label>
-                    <Col sm="10">
+                    <Col md={{ span: 4, offset: 4 }}>
                         <Form.Control type="text" />
                     </Col>
                 </Form.Group>
@@ -17,7 +17,7 @@ function BuyForm() {
                     <Form.Label column md={4}>
                         Cost USD:
                         </Form.Label>
-                    <Col sm="10">
+                    <Col md={{ span: 4, offset: 4 }}>
                         <Form.Control type="text" />
                     </Col>
                 </Form.Group>
@@ -25,7 +25,7 @@ function BuyForm() {
                     <Form.Label column md={4}>
                         Trans Fees:
                         </Form.Label>
-                    <Col sm="10">
+                    <Col md={{ span: 4, offset: 4 }}>
                         <Form.Control plaintext readOnly defaultValue="$5.00" />
                     </Col>
                 </Form.Group>
@@ -33,7 +33,7 @@ function BuyForm() {
                     <Form.Label column md={4}>
                         Total:
                         </Form.Label>
-                    <Col sm="10">
+                    <Col md={{ span: 4, offset: 4 }}>
                         <Form.Control plaintext readOnly defaultValue="$27.50" />
                     </Col>
                 </Form.Group>
