280b1d802ed104e2ed50da7654b5da1f94620ae9
Liam Stewart <liamchristopher.media@gmail.com>
2021-05-24 17:26:26 -0700

    finished Mining Hero component

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import Navigation from "./components/Navbar";
 import Accords from "./components/Accords";
 import Footer from "./components/Footer";
-import MiningHero from "./components/MiningHero";
 
 function App() {
   return (
     <>
       <Navigation />
       <Accords />
-      <MiningHero />
     </>
   );
 }
