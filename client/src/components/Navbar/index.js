6b46ac80b3904cccb6d47a5126b93060c6251b7f
Cheng Tang <cheng21tang@yahoo.com>
2021-05-25 15:02:30 -0700

    added font awesome CDN to index.html added little styling to Buttons on Navbar working on onClick handler

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Button, Nav } from "react-bootstrap";
 import Sidenav from "sidenavjs";
 import Sidebar from "../Sidebar";
 import SignUpForm from "../SignUp";
 import LoginForm from "../Login";
+import "./style.css";
 
 function Navigation() {
     const [sidebarOpen, setSidebarOpen] = useState(false)
@@ -15,18 +16,25 @@ function Navigation() {
 
     return (
         <>
-            <Navbar>
-                <Link className="navbar-brand" to="/">
-                    <img src="./assets/logo-25.svg" alt="brand-logo" />
-                    CryptoCoin
-                </Link>
-                <button onClick={() => { onSetSidebarOpen(true) }} type="button" id="sidebarToggle" className="btn btn-info">
-                    <img src="./assets/bars-solid.svg" alt="menu"></img>
-                </button>
+            <Navbar className="container-fluid">
+                <Nav.Item>
+                    <Link className="navbar-brand" to="/">
+                        <img src="./assets/logo-25.svg" alt="brand-logo" />
+                        CryptoCoin
+                    </Link>
+                </Nav.Item>
+                <Nav.Item className="d-flex">
+                    <Button id="signup">Sign-up</Button>
+                    <Button id="login">Login</Button>
+                    <Button id="menu" onClick={() => { onSetSidebarOpen(true) }} type="button" id="sidebarToggle" className="btn btn-info">
+                        {/* <img src="./assets/bars-solid.svg" alt="menu"></img> */}
+                        <i className="fas fa-bars"></i>
+                    </Button>
+                </Nav.Item>
             </Navbar>
             <Sidenav
                 open={sidebarOpen}
-                onSetOpen= {onSetSidebarOpen}
+                onSetOpen={onSetSidebarOpen}
                 sidenav={
                     // Passed in components here. Need to include conditional rendering when we have working logged-in status. Card has been made
                     <Sidebar />
