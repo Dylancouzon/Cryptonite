0119c252634fc9005e785b4b1b76540c6f64fd3a
Jake Novelli <jakenovelli11@gmail.com>
2021-05-25 13:28:46 -0700

    added transaction complete modal

--- client/src/pages/Buy.js
+++ client/src/pages/Buy.js
@@ -1,11 +1,13 @@
 import React from "react";
 import BuyForm from "../components/BuyForm";
+import TransComplete from "../components/TransComplete";
 
 
 function Buy() {
   return (
     <>
       <BuyForm />
+      <TransComplete />
     </>
   )
 }
