23f30310dd95eb3fbb9037b7cd2d7f0dc894722f
Dylancouzon <dylancouzon@gmail.com>
2021-05-26 19:38:35 -0700

    Fixed Concurently

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,23 @@
 const router = require('express').Router();
 const User = require('../models/users');
 
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
 //Not using the Controller as of right now.
 // const usersController = require("../controllers/usersController");
 
 
 router.post('/signUp', async (req, res) => {
-  console.log("test");
+  const key = ec.genKeyPair();
+  const publicKey = key.getPublic('hex');
+  const privateKey = key.getPrivate('hex');
   try {
-      console.log(req.body);
+    console.log(req.body);
+    req.body.public_key = publicKey;
     const userData = await User.create(req.body);
-    res.status(200).json(userData);
-  } catch(err) {
+    res.status(200).json(privateKey);
+  } catch (err) {
     res.status(500).json(err);
   }
 });
