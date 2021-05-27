b5b75495e635e0a3426be2c4293775794e27ecf4
Dylancouzon <dylancouzon@gmail.com>
2021-05-26 18:16:40 -0700

    Add concurrently

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,12 +1,31 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
 
+
+
 export default {
-    // Get book based on search
-    signUp: function(data) {
+    signUp: function (data) {
         console.log(data);
-        return axios.post(`http://localhost:${process.env.PORT}/api/signUp`, data);
-       
+        return axios.post(`/api/signUp`, data);
+
+
     },
 
 }
+
+// return axios.post(`http://localhost:3001/api/signUp`, data, {
+//     mode: 'cors',
+//     credentials: 'include'
+//   });
+
+// return axios({
+//     method: 'post', //you can set what request you want to be
+//     url: 'http://localhost:3001/api/signUp',
+//     data: data,
+//     crossDomain: true,
+//     headers: {
+//         'Access-Control-Allow-Origin': '*',
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//     }
+// })
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const User = require('../models/users');
 
+//Not using the Controller as of right now.
+// const usersController = require("../controllers/usersController");
+
+
 router.post('/signUp', async (req, res) => {
+  console.log("test");
   try {
       console.log(req.body);
     const userData = await User.create(req.body);
