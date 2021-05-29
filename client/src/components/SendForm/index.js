7f9d751a72286fb807c527a74d0b2aabc3bfba88
Jake Novelli <jakenovelli11@gmail.com>
2021-05-29 12:50:58 -0700

    added first part to send form

--- client/src/components/SendForm/index.js
+++ client/src/components/SendForm/index.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
 import { Form, Col, Row, Container, Button } from 'react-bootstrap';
+import SessionContext from "../../utils/sessionContext";
+import API from "../../utils/api";
+
 
 function SendForm() {
+    const formHandle = useRef();
+    const { publicKey } = useContext(SessionContext);
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        const data = [...formHandle.current].reduce((obj, input) => {
+            obj[input.getAttribute('id')] = input.value
+            return obj;
+        }, {})
+        data.fromAddress = publicKey;
+        console.log(data);
+        console.log(Object.values(data).reduce((a, c) => a && !!c, true));
+        if(!Object.values(data).reduce((result, value) => result && !!value, true)) {
+            alert("Fill out the entire form");
+            return;
+        }
+        console.log(formHandle.current);
+        console.log(formHandle.current[0].value);
+        API.sendTransaction(data)
+        .then(res => {
+            console.log(res);
+        })
+    }
+
     return (
         <Container>
-            <Form>
-                <Form.Group as={Row} controlId="formPlaintextRecipient">
+            <Form ref={formHandle}>
+                <Form.Group as={Row} controlId="toAddress">
                     <Col style={{marginTop: 5}} md={{ span: 7, offset: 2 }}>
                         <Form.Control type="text" placeholder="Recipient (Public Key)" />
                     </Col>
                 </Form.Group>
-                <Form.Group as={Row} controlId="formPlaintextPrivate">
+                <Form.Group as={Row} controlId="private">
                     <Col style={{marginTop: 5}} md={{ span: 7, offset: 2 }}>
                         <Form.Control type="text" placeholder="Private Key"/>
                     </Col>
                 </Form.Group>
-                <Form.Group as={Row} controlId="formPlaintextLabel">
+                <Form.Group as={Row} controlId="label">
                     <Col style={{marginTop: 5}} md={{ span: 7, offset: 2 }}>
                         <Form.Control type="text" placeholder="Label" />
                     </Col>
                 </Form.Group>
-                <Form.Group as={Row} controlId="formPlaintextPassword">
+                <Form.Group as={Row} controlId="amount">
                     <Form.Label style={{marginTop: 5}} column md={4}>
                         Amount of Coins:
                         </Form.Label>
@@ -28,7 +55,7 @@ function SendForm() {
                         <Form.Control type="text" />
                     </Col>
                 </Form.Group>
-                <Form.Group  as={Row} controlId="formPlaintextCost">
+                <Form.Group  as={Row} controlId="cost">
                     <Form.Label style={{marginTop: 5}} column md={4}>
                         Cost USD:
                         </Form.Label>
@@ -41,7 +68,7 @@ function SendForm() {
                         Trans Fees:
                         </Form.Label>
                     <Col style={{marginTop: 5}} md={{ span: 4, offset: 4 }}>
-                        <Form.Control plaintext readOnly defaultValue="$5.00" />
+                        <Form.Control plaintext readOnly defaultValue="5.00" />
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row} controlId="formPlaintextTotal">
@@ -49,15 +76,15 @@ function SendForm() {
                         Total:
                         </Form.Label>
                     <Col style={{marginTop: 5}} md={{ span: 4, offset: 4 }}>
-                        <Form.Control plaintext readOnly defaultValue="$27.50" />
+                        <Form.Control plaintext readOnly defaultValue="27.50" />
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row}>
                     <Col style={{marginTop: 5}} md={{ span: 10, offset: 5 }}>
-                        <Button type="submit">Continue</Button>
                     </Col>
                 </Form.Group>
             </Form>
+                <Button type="button" onClick={submitForm}>Continue</Button>
         </Container>
     )
 }
--- client/src/pages/Send.js
+++ client/src/pages/Send.js
@@ -1,10 +1,12 @@
 import React from "react";
 import WalletCoins from "../components/WalletCoins";
+import SendForm from "../components/SendForm";
 
 function Send() {
   return (
     <>
         <WalletCoins />
+        <SendForm />
     </>
   )
 }
