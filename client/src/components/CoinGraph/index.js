aba0f1cb9188b1b16a9dcef27bf25bbe7ccff605
Liam Stewart <liamchristopher.media@gmail.com>
2021-05-31 17:20:17 -0700

    added base graph for the coin graph

--- client/src/components/CoinGraph/index.js
+++ client/src/components/CoinGraph/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
 
-function CoinGraph() {
-    return(
-        <div>
-            <p>Graph about current coins</p>
-            <img src="./assets/images/ourCoin-ticker-placeholder.PNG" alt="our coin ticker" />
-        </div>
-    )
+const data = {
+    labels: ['1', '2', '3', '4', '5', '6'],
+    datasets: [
+        {
+            label: 'Coin Value',
+            data: [12, 19, 3, 5, 2, 3],
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgba(255, 99, 132, 0.2)',
+        },
+    ],
+};
+
+const options = {
+    scales: {
+        yAxes: [
+            {
+                ticks: {
+                    beginAtZero: true,
+                },
+            },
+        ],
+    },
+}
+
+
+    function CoinGraph() {
+        return (
+            <>
+                <div className='header'>
+                    <h1 className='title'>History</h1>
+                </div>
+                <Line data={data} options={options} />
+            </>
+        )
 }
 
 export default CoinGraph;
