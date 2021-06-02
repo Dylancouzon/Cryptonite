0f3a6f5cfc16a804dcc24dcc81bdaf30840436f8
Cheng Tang <cheng21tang@gmail.com>
2021-06-01 17:05:18 -0700

    added Alerts for Login to handle errors it is working as expected

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,11 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Form, Button } from "react-bootstrap";
+import Alert from 'react-bootstrap/Alert';
 import API from "../../utils/api";
 import SessionContext from "../../utils/sessionContext";
 
 function SignUpForm() {
-
     const { setSession } = useContext(SessionContext);
+
+    // Alert states and functions
+    const [showAlert, setShowAlert] = useState(false);
+    const [showAlertMessage, setShowAlertMessage] = useState("");
+    const handleCloseAlert = () => {
+        setShowAlert(false);
+        setShowAlertMessage("");
+    }
+    const handleAlertMessage = (message) => {
+        if(message) {
+            setShowAlert(true);
+            setShowAlertMessage(message);
+        }
+    }
+
     function handleSubmit(e) {
         e.preventDefault();
 
@@ -15,22 +30,37 @@ function SignUpForm() {
         }
         API.logIn(submitData)
         .then((res) => {
+            console.log(res) //use to debug
             var username = res.data.user[0].username; 
             var publicKey = res.data.user[0].public_key;
             var logged_in = true;
             setSession({username, publicKey, logged_in});
             document.location.replace("/");
-            console.log(res);
         })
         .catch((err) => {
-            // Error message, needs to be put inside an <Alert />
-            console.log(err.response.data.message);
+            console.log(err.response)
+            // console.log(err.response.data.message); //use to debug
+            switch(err.response.data.message) {
+                case "Server Error, Please try Again.":
+                    handleAlertMessage("Oops something went wrong. Please try again later.");
+                    break;
+                default:
+                    handleAlertMessage("Please check your username or password");
+            }
         });
     }
 
 
         return (
             <>
+                <Alert
+                    show={showAlert}
+                    variant="warning"
+                    dismissible="true"
+                    onClose={() => {handleCloseAlert()}}
+                >
+                    <p>{showAlertMessage}</p>
+                </Alert>
                 <div className="sidebar-header">
                     <h3>Login</h3>
                 </div>
