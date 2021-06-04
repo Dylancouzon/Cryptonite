e56b713c56174e6d8ae58eb4937b1d9f51d31ceb
Cheng Tang <cheng21tang@gmail.com>
2021-06-04 13:30:02 -0700

    pushing last minor changes

--- client/src/components/LearnMore/index.js
+++ client/src/components/LearnMore/index.js
@@ -9,10 +9,7 @@ const body = (
   <Row>
     <Col md={6}><img src={'https://cdn.pixabay.com/photo/2018/01/17/20/22/analytics-3088958_960_720.jpg'} className="image" alt='Learn More' /></Col>
     <Col md={6} className="cardBody" id="learnCard"> <h1>{title}</h1>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-      when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-      into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-     Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>Blockchain is a shared, immutable ledger that facilitates the process of recording transactions and tracking assets in a business network. An asset can be tangible (a house, car, cash, land) or intangible (intellectual property, patents, copyrights, branding). Virtually anything of value can be tracked and traded on a blockchain network, reducing risk and cutting costs for all involved.</p>
     </Col>
 
   </Row>
