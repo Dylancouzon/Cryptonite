a1e6f24056fb378e52f9af74475b2dc760e0cd28
Cheng Tang <cheng21tang@gmail.com>
2021-06-06 17:35:00 -0700

    updated href _blank for creator links

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -10,10 +10,10 @@ function Footer() {
                 <Card.Body>
                     <Card.Text>
                         Cryptonite created by : 
-                        <a href="https://github.com/cheng21tang" target="blank"> Cheng Tang </a> | 
-                        <a href="https://github.com/Dylancouzon" target="blank"> Dylan Couzon </a> |
-                        <a href="https://github.com/dnovelli1" target="blank"> Jake Novelli</a> |
-                        <a href= "https://github.com/LiamStewart8" target="blank"> Liam Stewart</a>
+                        <a href="https://github.com/cheng21tang" target="_blank" rel="noreferrer"> Cheng Tang </a> | 
+                        <a href="https://github.com/Dylancouzon" target="_blank" rel="noreferrer"> Dylan Couzon </a> |
+                        <a href="https://github.com/dnovelli1" target="_blank" rel="noreferrer"> Jake Novelli</a> |
+                        <a href= "https://github.com/LiamStewart8" target="_blank" rel="noreferrer"> Liam Stewart</a>
                     </Card.Text>
                 </Card.Body>
             </Card>
